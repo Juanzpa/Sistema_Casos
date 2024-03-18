8705d6befd1a5850c72c0c23b304f274cf2bd127
Juanjo <juanjose.ortizaquino234@gmail.com>
2024-03-17 23:46:40 -0600

    tabla de programadores sin datos
    tabla de programadores sin datos

--- src/sv/edu/udb/sistemas/Administrador/menuAdmin.java
+++ src/sv/edu/udb/sistemas/Administrador/menuAdmin.java
@@ -1,11 +1,17 @@
 package sv.edu.udb.sistemas.Administrador;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class menuAdmin extends JFrame {
     private JPanel pnlmenuadmin;
     private JTabbedPane tabbedPane1;
+    private JLabel lblTituloProgramador;
+    private JTable tblProgramadores;
+    DefaultTableModel modeloPogramadores = null;
+    String[] columnaProgramadores;
+    Object[][] datosProgramador;
 
     public menuAdmin(String title) {
         super(title);
@@ -13,6 +19,12 @@ public class menuAdmin extends JFrame {
         this.setContentPane(pnlmenuadmin);
         this.setMinimumSize(new Dimension(600, 500));
         this.setLocationRelativeTo(getParent());
+
+        columnaProgramadores = new String[] {"Nombre", "Apellido", "Cargo"};
+        datosProgramador = new Object[][] {};
+        modeloPogramadores = new DefaultTableModel(datosProgramador, columnaProgramadores);
+        tblProgramadores.setModel(modeloPogramadores);
+
     }
     public static void main(String[] args) {
         JFrame frama = new menuAdmin("menuAdmin");
