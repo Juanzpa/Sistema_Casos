fc1b80dd544e49f710313b3bd8fbda8e05f7f991
Juanjo <juanjose.ortizaquino234@gmail.com>
2024-03-19 22:34:26 -0600

    vista empleado
    vista empleado

--- src/sv/edu/udb/sistemas/EmpleadoAreaFuncional/EmpleadoProbador.java
+++ src/sv/edu/udb/sistemas/EmpleadoAreaFuncional/EmpleadoProbador.java
@@ -1,4 +1,41 @@
 package sv.edu.udb.sistemas.EmpleadoAreaFuncional;
 
-public class EmpleadoProbador {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class EmpleadoProbador extends JFrame {
+
+    private JTabbedPane tbdEmpleado;
+    private JLabel lblCasosEmpleado;
+    private JComboBox cmbCasosEmpleado;
+    private JTable tblCasosEmpleado;
+    private JButton btnRechazarCasoEmp;
+    private JButton btnAceptarCasoEmp;
+    private JLabel lblBitacoraEmpleado;
+    private JComboBox cmbBitacorasEmpleado;
+    private JTable tblBitacoraEmpleado;
+    private JPanel pnlEmpleado;
+
+    public EmpleadoProbador(String panel){
+        super(panel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(pnlEmpleado);
+        this.setMinimumSize(new Dimension(600, 500));
+        this.setLocationRelativeTo(getParent());
+
+        btnRechazarCasoEmp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tbdEmpleado.setSelectedIndex(1);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new EmpleadoProbador("panel");
+        frame.setVisible(true);
+    }
 }
